8bf61278ab558b92d24d911f79717a076fd162af
GrAlexandrov <alexandrov.18rus@gmail.com>
2024-12-18 11:43:49 +0400

    ДЗ Activities/Homework/activity_02 поправка finish

--- receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
+++ receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
@@ -1,25 +1,31 @@
 package otus.gpb.homework.activities.receiver
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+const val titleKey = "titleKey"
+const val titleyear = "titleyear"
+const val titledescription = "titledescription"
+
 class ReceiverActivity : AppCompatActivity() {
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receiver)
 
-        val title= intent.extras?.getString("titleKey")
+        val title= intent.extras?.getString(titleKey)
         findViewById<TextView>(R.id.titleTextView).text=title
 
-        val titleyear= intent.extras?.getString("titleyear")
+        val titleyear= intent.extras?.getString(titleyear)
         findViewById<TextView>(R.id.yearTextView).text=titleyear
 
-        val titledescription= intent.extras?.getString("titledescription")
+        val titledescription= intent.extras?.getString(titledescription)
         findViewById<TextView>(R.id.descriptionTextView).text=titledescription
 
-        findViewById<ImageView>(R.id.posterImageView).setImageDrawable(this.getDrawable (R.drawable.interstellar))
+        findViewById<ImageView>(R.id.posterImageView).setImageDrawable(getDrawable (R.drawable.interstellar))
     }
 }
